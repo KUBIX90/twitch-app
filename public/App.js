71ed3d104f7f539c28da101027f35f28c9eed864
KUBIX90 <mark.j.kubik@gmail.com>
2017-12-29 22:56:10 +0000

    basic channel layout implemented

--- public/App.js
+++ public/App.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	
-	var twitchAPI = "https://wind-bow.gomix.me/twitch-api/users/freecodecamp";
+	var twitchUser = "https://wind-bow.gomix.me/twitch-api/users/";
+	    twitchStream = "https://wind-bow.gomix.me/twitch-api/streams/";
+		twitchChannel = "https://wind-bow.gomix.me/twitch-api/channels/";
 	
 	var user = ["ESL_SC2", "OgamingSC2", "cretetion", 
 				"freecodecamp", "storbeck", "habathcx", 
 				"RobotCaleb", "noobs2ninjas"]
 	
 	$.ajax({
-		  url: twitchAPI,
+		  url: twitchChannel + user[3],
 		  data: {
 			format: "json",
 		  },
@@ -15,6 +17,13 @@ $(document).ready(function(){
 		  type: "GET",
 		  success: function(data) {
 			  console.log(data);
+			  $("#logo").attr({src: data.logo,
+							   alt: data.display_name});
+			  $("#title").html(data.display_name)
+						 .attr({"href": data.url,
+								"target": "_blank"});
+			  $("#description").html(data.status);
+			  $("#followers").html(data.followers);
 		  }
 	});
 });
