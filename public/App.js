65e4aea5d5339b2f72822d845086e04ed7f5d9d7
KUBIX90 <mark.j.kubik@gmail.com>
2018-01-02 22:44:42 +0000

    for loop used to display all channels upon loading page

--- public/App.js
+++ public/App.js
@@ -1,29 +1,39 @@
 $(document).ready(function(){
 	
-	var twitchUser = "https://wind-bow.gomix.me/twitch-api/users/";
-	    twitchStream = "https://wind-bow.gomix.me/twitch-api/streams/";
-		twitchChannel = "https://wind-bow.gomix.me/twitch-api/channels/";
+	var twitchUser = "https://wind-bow.glitch.me/twitch-api/users/";
+	    twitchStream = "https://wind-bow.glitch.me/twitch-api/streams/";
+		twitchChannel = "https://wind-bow.glitch.me/twitch-api/channels/";
 	
 	var user = ["ESL_SC2", "OgamingSC2", "cretetion", 
 				"freecodecamp", "storbeck", "habathcx", 
-				"RobotCaleb", "noobs2ninjas"]
+				"RobotCaleb", "noobs2ninjas"];
 	
-	$.ajax({
-		  url: twitchChannel + user[3],
+	function getChannels(){
+	for (var i = 0; i < user.length; i++){	
+		$.ajax({
+		  url: twitchChannel + user[i],
 		  data: {
 			format: "json",
 		  },
 		  dataType: "jsonp",
 		  type: "GET",
 		  success: function(data) {
-			  console.log(data);
-			  $("#logo").attr({src: data.logo,
-							   alt: data.display_name});
-			  $("#title").html(data.display_name)
-						 .attr({"href": data.url,
-								"target": "_blank"});
-			  $("#description").html(data.status);
-			  $("#followers").html(data.followers);
-		  }
-	});
+			
+		$("#twitch_channels").append(
+			"<article class = 'flex--aligned'>" +
+				"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
+					"<section id = 'text'>" +
+						"<a id = 'title'" + "href = " + data.url + "target = _blank>" + data.display_name + "</a>" +
+						"<p id = 'description'>" + data.status + "</p>" +
+						"<p id = 'followers'>" + data.followers + "</p>" +
+					"</section>" +
+					"<img id = 'logo2'/>" +	
+			"</article>"
+			);
+		}
+	});	
+	}
+	};
+	
+	getChannels();
 });
