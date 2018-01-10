063f40486e8fd14a113cb11a678a81b3780c754e
KUBIX90 <mark.j.kubik@gmail.com>
2018-01-10 20:32:25 +0000

    API call amended to a get request

--- public/App.js
+++ public/App.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-	
 	var twitchUser = "https://wind-bow.glitch.me/twitch-api/users/";
 	    twitchStream = "https://wind-bow.glitch.me/twitch-api/streams/";
 		twitchChannel = "https://wind-bow.glitch.me/twitch-api/channels/";
@@ -8,32 +6,47 @@ $(document).ready(function(){
 				"freecodecamp", "storbeck", "habathcx", 
 				"RobotCaleb", "noobs2ninjas"];
 	
-	function getChannels(){
-	for (var i = 0; i < user.length; i++){	
-		$.ajax({
-		  url: twitchChannel + user[i],
-		  data: {
-			format: "json",
-		  },
-		  dataType: "jsonp",
-		  type: "GET",
-		  success: function(data) {
-			
-		$("#twitch_channels").append(
-			"<article class = 'flex--aligned'>" +
-				"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
-					"<section id = 'text'>" +
-						"<a id = 'title'" + "href = " + data.url + "target = _blank>" + data.display_name + "</a>" +
-						"<p id = 'description'>" + data.status + "</p>" +
-						"<p id = 'followers'>" + data.followers + "</p>" +
-					"</section>" +
-					"<img id = 'logo2'/>" +	
-			"</article>"
-			);
-		}
-	});	
-	}
-	};
 	
-	getChannels();
+	$.get(twitchUser + user[2], function(data){
+		console.log(data);
+		$.get(twitchStream + user[2], function(val){
+		console.log(val);
+		})
+	});
+	
+	
+			/*$("#twitch_channels").append(
+				"<article class = 'flex--aligned'>" +
+					"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
+						"<section id = 'text'>" +
+							"<a id = 'title'" + "href = " + data.url + " target = _blank>" + data.display_name + "</a>" +
+							"<p id = 'description'>" + data.status + "</p>" +
+							"<p id = 'followers'>" + data.followers + "</p>" +
+						"</section>" +
+						"<img id = 'logo2'/>" +	
+				"</article>"
+			);*/
+					
+	
+
+	
+//////////////////////////////////////////////////////////////////
+
+$(document).ready(function(){
+	//getChannels();
+	
+	
+	//make another ajax call to get online status from streamAPI (check this) and make sure its placed correctly
+	
+	//call get channels function upon clicking the all button
+	
+	//show online/offline on click, will most likely need to use the streamsAPI again for this
+	
+	//get search bar showing relevant channels upon typing
+	
+	//move search bar between button and channels, center everything
+	
+	//style CSS accordingly
+	
+	//Speed up javascript (check quick way to append and any other best practices)
 });
