658ab7a1404743950b55d54c80be0cff4753e558
KUBIX90 <mark.j.kubik@gmail.com>
2018-01-11 21:06:52 +0000

    channels display on load and on click

--- public/App.js
+++ public/App.js
@@ -6,47 +6,35 @@
 				"freecodecamp", "storbeck", "habathcx", 
 				"RobotCaleb", "noobs2ninjas"];
 	
-	
-	$.get(twitchUser + user[2], function(data){
-		console.log(data);
-		$.get(twitchStream + user[2], function(val){
-		console.log(val);
-		})
-	});
-	
-	
-			/*$("#twitch_channels").append(
-				"<article class = 'flex--aligned'>" +
-					"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
+function showChannels(){
+	for (var i = 0; i < user.length; i++){
+		$.get(twitchChannel + user[i], function(data){
+			$.get(twitchStream + user[i], function(val){
+				$("#twitch_channels").append(
+					"<article class = 'flex--aligned'>" +
+						"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
 						"<section id = 'text'>" +
 							"<a id = 'title'" + "href = " + data.url + " target = _blank>" + data.display_name + "</a>" +
 							"<p id = 'description'>" + data.status + "</p>" +
-							"<p id = 'followers'>" + data.followers + "</p>" +
+							"<p id = 'followers'>" + "Followers " + data.followers + "</p>" +
 						"</section>" +
-						"<img id = 'logo2'/>" +	
-				"</article>"
-			);*/
-					
-	
+					"</article>"
+				);
+			})
+		})
+	};
+};
 
 	
 //////////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
-	//getChannels();
-	
-	
-	//make another ajax call to get online status from streamAPI (check this) and make sure its placed correctly
-	
-	//call get channels function upon clicking the all button
-	
-	//show online/offline on click, will most likely need to use the streamsAPI again for this
-	
-	//get search bar showing relevant channels upon typing
-	
-	//move search bar between button and channels, center everything
-	
-	//style CSS accordingly
-	
-	//Speed up javascript (check quick way to append and any other best practices)
+
+	showChannels();
+
+	$("#channels_all").click(function(){
+		$("#twitch_channels").empty();
+		showChannels();
+	});
+
 });
