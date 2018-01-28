73bfdcedbb740bf04a1bce62f916128f324d32ac
KUBIX90 <mark.j.kubik@gmail.com>
2018-01-28 18:51:38 +0000

    mobile responsiveness added

--- public/App.js
+++ public/App.js
@@ -1,18 +1,19 @@
-	var twitchUser = "https://wind-bow.glitch.me/twitch-api/users/";
-	    twitchStream = "https://wind-bow.glitch.me/twitch-api/streams/";
-		twitchChannel = "https://wind-bow.glitch.me/twitch-api/channels/";
-	
-	var user = ["ESL_SC2", "OgamingSC2", "cretetion", 
-				"freecodecamp", "Miss_Rage", "Pandagirl91", 
-				"RobotCaleb", "noobs2ninjas"];
-	
+var twitchUser = "https://wind-bow.glitch.me/twitch-api/users/";
+	twitchStream = "https://wind-bow.glitch.me/twitch-api/streams/";
+	twitchChannel = "https://wind-bow.glitch.me/twitch-api/channels/";
+
+var user = ["ESL_SC2", "OgamingSC2", "cretetion", 
+			"freecodecamp", "Miss_Rage", "Pandagirl91", 
+			"RobotCaleb", "noobs2ninjas"];
+
 function showChannel(username){
 		$.get(twitchChannel + username, function(data){
+			var userTitle = function 
 				$("#twitch_channels").append(
 					"<article id = " + username + " class = 'flex channel--margin flex--aligned-center'>" +
 						"<img id = 'logo'" + "src = '" + data.logo + "' alt = '" + data.display_name + "'/>" +
 						"<section id = 'text'>" +
-							"<a id = 'title'" + "href = " + data.url + " target = _blank>" + data.display_name + "</a>" +
+							"<a id = 'title'" + "href = " + data.url + " target = _blank " + "at = " + data.display + ">" + data.display_name + "</a>" +
 							"<p id = 'description'>" + data.game + "</p>" +
 							"<p id = 'followers'>" + "Followers " + data.followers + "</p>" +
 						"</section>" +
@@ -53,6 +54,7 @@ function showOfflineChannels(username){
 //////////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
+	
 	for (i = 0; user.length > i; i ++){
 		showChannel(user[i]);
 	}
@@ -75,3 +77,11 @@ $("#channels_offline").click(function(){
 		showOfflineChannels(user[i]);
 	}
 });
+
+//Search channels upon typing
+$("#search-box").keyup(function(){
+	var inputValue = $("#search-box").val().toLowerCase();
+	
+	//Get title text and compare to input value?
+});
+
