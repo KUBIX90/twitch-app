4839478090ab6169b355bc0011e6ad7712801ba7
KUBIX90 <mark.j.kubik@gmail.com>
2018-02-01 21:59:51 +0000

    removed API calls from channel buttons

--- public/App.js
+++ public/App.js
@@ -1,83 +1,69 @@
-var twitchUser = "https://wind-bow.glitch.me/twitch-api/users/";
-	twitchStream = "https://wind-bow.glitch.me/twitch-api/streams/";
-	twitchChannel = "https://wind-bow.glitch.me/twitch-api/channels/";
+var twitchStreamAPI = "https://wind-bow.glitch.me/twitch-api/streams/";
+	twitchChannelAPI = "https://wind-bow.glitch.me/twitch-api/channels/";
 
-var user = ["ESL_SC2", "OgamingSC2", "cretetion", 
+var user = ["ESL_SC2", "LegendaryLea", "cretetion", 
 			"freecodecamp", "Miss_Rage", "Pandagirl91", 
 			"RobotCaleb", "noobs2ninjas"];
-	
-function showChannel(username){
-		$.get(twitchChannel + username, function(data){
+
+function twitchChannel(username){
+		$.get(twitchChannelAPI + username, function(data){
 				$("#twitch_channels").append(`
 				<article id = '${username}' class = 'flex flex--aligned-center channel--margin'>
 					<img id = 'channel_icon' src = '${data.logo}' alt = 'data.display_name'/>
 					<section id = 'channel_info'>
 						<a id = 'channel_title' href = ${data.url} target = '_blank'>${data.display_name}</a>
 						<p id = 'channel_description'>${data.game}</p>
-						<p id = 'channel_followers'>Followers " ${data.followers}</p>
+						<p id = 'channel_followers'>Followers ${data.followers}</p>
 					</section>
 					<p></p>
 				</article>`);
-			$.get(twitchStream + username, function(val){		
+			$.get(twitchStreamAPI + username, function(val){		
 				if(val.stream === null){
-                    $("#" + username).css("box-shadow", "5px 0px 10px red");
-                    $("#" + username + ">p").text("Offline")
-											.css("color","red");
-                } else {$("#" + username).css("box-shadow", "5px 0px 10px green");
-                        $("#" + username + ">p").text("Online")
-                                    .css("color","green");
+                    $("#" + username).css("box-shadow", "5px 0px 10px red")
+									 .addClass("channel--offline");
+                    $("#" + username + ">p")
+					.text("Offline")
+					.css("color","red")
+                } else {
+					$("#" + username).css("box-shadow", "5px 0px 10px green")
+									 .addClass("channel--online");
+                    $("#" + username + ">p")
+							.text("Online")
+                            .css("color","green")
                         }
 			})
 		})
 };
-
-function showOnlineChannels(username){
-	$("#" + username).removeClass("channel--display-none");
-	$.get(twitchStream + username, function(data){
-		if(data.stream === null){
-			$("#" + username).addClass("channel--display-none");
-		}
-	});
-}
-
-function showOfflineChannels(username){
-	$("#" + username).removeClass("channel--display-none");
-	$.get(twitchStream + username, function(data){
-		if(data.stream !== null){
-			$("#" + username).addClass("channel--display-none");
-		}
-	});
-}
 	
 //////////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
 	for (i = 0; user.length > i; i ++){
-		showChannel(user[i]);
+		twitchChannel(user[i]);
 	}
 });
 
 $("#channels_all").click(function(){
-	for (i = 0; user.length > i; i ++){
-		$("#" + user[i]).removeClass("channel--display-none");
-	};
+	$('.channel--online, .channel--offline').show();
 });
 
 $("#channels_online").click(function(){
-	for(i = 0; user.length > i; i ++){
-		showOnlineChannels(user[i]);
-	}
+	$('.channel--online').show();
+    $('.channel--offline').hide();
 });
 
 $("#channels_offline").click(function(){
-	for(i = 0; user.length > i; i ++){
-		showOfflineChannels(user[i]);
-	}
+	$('.channel--offline').show();
+    $('.channel--online').hide();
 });
 
 $("#search-box").keyup(function(){
-	var inputValue = $("#search-box").val().toLowerCase();
-	var userLowerCase = user.map(function(x){return x.toLowerCase();});
+  var inputValue = $("#search-box")
+	.val()
+    .toLowerCase();
+  var userLowerCase = user.map(function(x) {
+    return x.toLowerCase();
+  });
 	
 	for(i = 0; user.length > i; i ++){
 		if(userLowerCase[i].indexOf(inputValue) !== -1){
